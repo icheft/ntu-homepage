4104c42956ef7039a4e47cbd2a8e1cfcb684c31d
Brian Chen <brian.lxchen@gmail.com>
2020-05-27 20:39:59 +0800

    change color

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,23 @@ function logClick (e) {
 
 //show clicks
 function showClicks () {
-    if (clickCnt <= 10) {
-        clickCounter.innerHTML = `Hooray! You've starred ${clickCnt} times 🙂`;
+    if (clickCnt == 1) {
+        clickCounter.innerHTML = `Yes! You've starred ${clickCnt} time 🙂`;
+    }
+    else if (clickCnt <= 10) {
+        clickCounter.innerHTML = `Hooray! You've starred ${clickCnt} times 😁`;
     }
     else if (clickCnt > 10 && clickCnt <= 20) {
-        clickCounter.innerHTML = `Awesome! You've starred ${clickCnt} times 🎉`;
+        clickCounter.innerHTML = `Awesome! You've starred ${clickCnt} times 🎉<br>How about starring me on <a href="https://github.com/icheft/ntu-homepage" target="_blank">GitHub</a>?`;
     }
     else if (clickCnt > 20 && clickCnt <= 40) {
-        clickCounter.innerHTML = `Let's goooo! You've starred ${clickCnt} times 🍾`;
+        clickCounter.innerHTML = `Let's goooo! You've starred ${clickCnt} times 🍾<br>How about starring me on <a href="https://github.com/icheft/ntu-homepage" target="_blank">GitHub</a>?`;
     }
     else if (clickCnt > 40 && clickCnt <= 100) {
-        clickCounter.innerHTML = `That's lit 🔥 You've starred ${clickCnt} times, but you should definitely stop starring 💁🏾‍♂️`;
+        clickCounter.innerHTML = `That's lit 🔥 You've starred ${clickCnt} times, but you should definitely stop starring 💁🏾‍♂️<br>How about starring me on <a href="https://github.com/icheft/ntu-homepage" target="_blank">GitHub</a>?`;
     }
     else if (clickCnt > 100) {
-        clickCounter.innerHTML = `OMG 😱 You've starred ${clickCnt} times 🤯 Screen-capture this message and DM me 😍 You'll get nothing as a reward!`;
+        clickCounter.innerHTML = `OMG 😱 You've starred ${clickCnt} times 🤯 Screen-capture this message and DM me 😍<br>You'll get nothing as a reward 🤪<br>But how about starring me on <a href="https://github.com/icheft/ntu-homepage" target="_blank">GitHub</a>?`;
     }
 }
 
